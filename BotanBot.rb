0796f2844928b0e6dfae82b4425c32f284df616f
Matthew Yarnot <rot8er.conex@gmail.com>
2019-01-27 04:06:19 -0600

    Update BotanBot.rb

--- BotanBot.rb
+++ BotanBot.rb
@@ -11,6 +11,10 @@ require 'rufus-scheduler'              # Download link: https://github.com/jmett
 require 'active_support/core_ext/time' # Download link: https://rubygems.org/gems/activesupport/versions/5.0.0
 require_relative 'rot8er_functs'       # functions I use commonly in bots
 
+# this is required to get her to post messages in a certain server when specific criteria are met
+ENV['TZ'] = 'America/Chicago'
+@scheduler = Rufus::Scheduler.new
+
 @prefix=['DL!','Dl!','dL!','dl!',
          'DL?','Dl?','dL?','dl?']
 
