6b219999b7e2f0125f4c76bb210032cdeaa140c5
Matthew Yarnot <rot8er.conex@gmail.com>
2019-02-17 15:57:52 -0600

    Update BotanBot.rb

--- BotanBot.rb
+++ BotanBot.rb
@@ -5613,6 +5613,12 @@ bot.message do |event|
     elsif event.server.nil? || event.server.id==285663217261477889
       event.respond "I am not Liz right now.  Please use `FGO!reboot` to turn me into Liz."
     end
+  elsif (['!weak '].include?(str[0,6]) || ['!weakness '].include?(str[0,10]))
+    if event.server.nil? || event.server.id==264445053596991498
+    elsif !bot.user(304652483299377182).on(event.server.id).nil? # Robin
+    elsif !bot.user(206147275775279104).on(event.server.id).nil? || @shardizard==4 || event.server.id==330850148261298176 # Pokedex
+      triple_weakness(bot,event)
+    end
   elsif overlap_prevent(event)
   elsif m && !all_commands().include?(s.split(' ')[0])
     if find_data_ex(:find_adventurer,s,event,true).length>0
