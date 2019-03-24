65857bf1fdf8b350b792480e5131d4b30c5a684b
Matthew Yarnot <rot8er.conex@gmail.com>
2019-03-24 12:53:22 -0500

    Update BotanBot.rb

--- BotanBot.rb
+++ BotanBot.rb
@@ -20,16 +20,13 @@ ENV['TZ'] = 'America/Chicago'
 load 'C:/Users/Mini-Matt/Desktop/devkit/DLPrefix.rb'
 
 prefix_proc = proc do |message|
-  next message.text.downcase[4..-1] if message.text.downcase.start_with?('dl! ')
-  next message.text.downcase[4..-1] if message.text.downcase.start_with?('dl? ')
-  next message.text.downcase[3..-1] if message.text.downcase.start_with?('dl!')
-  next message.text.downcase[3..-1] if message.text.downcase.start_with?('dl?')
+  next pseudocase(message.text[3..-1]) if message.text.downcase.start_with?('dl!')
+  next pseudocase(message.text[3..-1]) if message.text.downcase.start_with?('dl?')
   load 'C:/Users/Mini-Matt/Desktop/devkit/DLPrefix.rb'
   next if message.channel.server.nil? || @prefixes[message.channel.server.id].nil? || @prefixes[message.channel.server.id].length<=0
   prefix = @prefixes[message.channel.server.id]
   # We use [prefix.size..-1] so we can handle prefixes of any length
-  next message.text.downcase[prefix.size+1..-1] if message.text.downcase.start_with?("#{prefix.downcase} ")
-  next message.text.downcase[prefix.size..-1] if message.text.downcase.start_with?(prefix.downcase)
+  next pseudocase(message.text[prefix.size..-1]) if message.text.downcase.start_with?(prefix.downcase)
 end
 
 # The bot's token is basically their password, so is censored for obvious reasons
