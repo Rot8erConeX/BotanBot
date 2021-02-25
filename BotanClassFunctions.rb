183c934efc6e56c4af50cd1c80ab6fbe8418f655
Matthew Yarnot <rot8er.conex@gmail.com>
2021-02-25 06:05:02 -0600

    Add files via upload

--- BotanClassFunctions.rb
+++ BotanClassFunctions.rb
@@ -950,7 +950,7 @@ class DLFacility
     elsif @subtype=='Altar'
       alta=[(lvl+1)/2,lvl/2]
       alta=[lvl-17,lvl-17] if lvl>34
-      alta=[lvl/2+2,(lvl+3)/2] if lvl>40
+      alta=[(lvl+1)/2,lvl/2] if lvl>40
       alta=alta.map{|q| q*0.5+0.5}
       return alta
     elsif ['Dojo','Fafnir'].include?(@subtype)
