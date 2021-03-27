ecc77f4b17cd084f74a9d3d167e81870a4ce8062
Matthew Yarnot <rot8er.conex@gmail.com>
2021-03-27 08:24:00 -0500

    Add files via upload

--- BotanClassFunctions.rb
+++ BotanClassFunctions.rb
@@ -1338,6 +1338,18 @@ def disp_dragon_stats(bot,event,args=nil,juststats=false,preload=nil)
       d.thumbforce="Adventurers/Gala_Mym_5" if mym=='Gala'
       disp_dragon_stats(bot,event,args,juststats,d)
       return nil
+    elsif (has_any?(args,['human','humanoid','man']) && has_any?(args,['midgardsormr','middy','midgard'])) || has_any?(args,['humanoidmidgardsormr','humiddy','humangard','humangardsormar','mangard','mangardsormar'])
+      d=drg.find_index{|q| q.name=='Midgardsormr'}
+      d=drg[d].clone
+      a=adv.find_index{|q| q.name=='Humanoid Midgardsormr'}
+      d.rarity="#{adv[a].rarity}#{adv[a].availability}"
+      d.name='Midgardsormr (not-humanoid)'
+      d.hp='0, 0'; d.str='0, 0'; d.sell_price='0, 0'
+      d.skills='Calamitous Tempest'
+      d.auras=''
+      d.thumbforce='Dragons/Midgardsormr'
+      disp_dragon_stats(bot,event,args,juststats,d)
+      return nil
     elsif (has_any?(args,['shiny']) && has_any?(args,['nyarlathotep'])) || has_any?(args,['shinynyarlathotep','lathna'])
       d=drg.find_index{|q| q.name=='Nyarlathotep'}
       d=drg[d].clone
