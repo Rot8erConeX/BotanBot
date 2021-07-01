79139fae67038cf507ff4401092407096e26f90e
Matthew Yarnot <rot8er.conex@gmail.com>
2021-06-30 20:43:41 -0500

    Add files via upload

--- BotanClassFunctions.rb
+++ BotanClassFunctions.rb
@@ -7666,6 +7666,8 @@ def snagstats(event,bot,f=nil,f2=nil)
   nicknames_load()
   data_load()
   metadata_load()
+  system "gem uninstall discordrb"
+  system "gem install discordrb"
   f='' if f.nil?
   f2='' if f2.nil?
   bot.servers.values(&:members)
